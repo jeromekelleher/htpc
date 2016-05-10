d0b65d193b8f940e8a180eb8e9746568b1832165
Jerome Kelleher <jk@well.ox.ac.uk>
2016-05-10 21:03:10 +0100

    Fixed issues with running as hts user.

--- get_tv_listings.sh
+++ get_tv_listings.sh
@@ -2,9 +2,10 @@
 # Script to be run daily to collect tv listings and 
 # send them to tvheadend.
 
-SOCK_PATH=/home/hts/.hts/tvheadend/epggrab/xmltv.sock
 # Become the hts user so we can get the correct config.
 # We need to delete the second line in the output xml 
 # as it causes problems with tvheadend (DOCTYPE).
 
-su hts -c tv_grab_uk_atlas | sed -e 2d | socat - UNIX-CONNECT:$SOCK_PATH
+# Get one days worth of listings for 1 days in the future
+sudo -u hts -H sh -c \
+    'tv_grab_uk_atlas --days=1 --offset=1 | sed -e 2d | socat - UNIX-CONNECT:/home/hts/.hts/tvheadend/epggrab/xmltv.sock'
